c8f51855f645b4147d6fc80f0f436ab05e5cf6d3
tsh <dr.tallin@gmail.com>
2016-02-13 19:19:37 +0200

    simple json/post server

--- hello_world.go
+++ hello_world.go
@@ -3,12 +3,41 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+	"encoding/json"
 )
 
+type User struct {
+	FirstName 	string `json:"first_name"`
+	LastName 	string `json:"last_name"`
+	Email 		string `json:"email"`
+	CreatedAt 	time.Time `json:"created_at"`
+}
+
+type HomePageHandler struct{}
+
+func (h *HomePageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	user := new(User)
+	json.NewDecoder(r.Body).Decode(user)
+	user.CreatedAt = time.Now()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	data, _ := json.Marshal(user)
+	w.Write(data)
+}
+
 func main(){
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprint(w, "Hello World!")
-	})
+	http.Handle("/", &HomePageHandler{})
+
+	barHandler := func (w http.ResponseWriter, r *http.Request){
+		name := r.URL.Query().Get("name")
+		if name == "" {
+			name = "Bar"
+		}
+		fmt.Fprintf(w, "Hello %s!", name )
+	}
+	http.HandleFunc("/bar", barHandler)
 
 	http.ListenAndServe(":3000", nil)
 }
