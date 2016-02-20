eb0e907c8f728b86e44911f9cb76ed9defff6b39
tsh <dr.tallin@gmail.com>
2016-02-20 17:41:10 +0200

    json test

--- tests/my_app/my_app_test.go
+++ tests/my_app/my_app_test.go
@@ -7,6 +7,8 @@ import (
 	"../my_app"
 
 	"github.com/stretchr/testify/assert"
+	"strings"
+	"encoding/json"
 )
 
 func TestHomePageHandler(t *testing.T) {
@@ -43,4 +45,22 @@ func TestWelcomeByNameHandler_WithName(t *testing.T) {
 
 	assert.Equal(res.Code, 200)
 	assert.Equal(res.Body.String(), "Hello Mark!")
+}
+
+
+func TestJsonHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	mux := my_app.NewMux()
+	res := httptest.NewRecorder()  // mock
+	req, _ := http.NewRequest("POST", "/json", strings.NewReader(`{"first_name": "tsh"}`))
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(res.Code, 201)
+	assert.Equal(res.Header().Get("Content-Type"), "application/json")
+	user := new(my_app.User)
+	json.NewDecoder(res.Body).Decode(user)
+
+	assert.Equal(user.Id, 1)
+	assert.Equal(user.FirstName, "tsh")
 }
